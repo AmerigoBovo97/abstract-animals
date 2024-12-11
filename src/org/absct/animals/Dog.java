aff65d6eb9c9d71775b26def1ffaa0ee0a6c5e6f
AmerigoBovo97 <amerigobovo@gamil.com>
2024-12-11 21:57:22 +0100

    feat: implement Dog class

--- src/org/absct/animals/Dog.java
+++ src/org/absct/animals/Dog.java
@@ -1,5 +1,15 @@
 package org.absct.animals;
 
-public class Dog {
+public class Dog extends Animal{
     
+    @Override
+    public void sound(){
+        System.out.println("Bau!!");
+    }
+
+    @Override
+    public void eat(){
+        System.out.println("Crocchette e wurstel");
+    }
+
 }
