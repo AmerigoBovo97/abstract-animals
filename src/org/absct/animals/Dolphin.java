2d4c3a6ef6bf8ed8458489755d25df54fef6b68e
AmerigoBovo97 <amerigobovo@gamil.com>
2024-12-11 22:08:48 +0100

    feat: implement Eagles and Dolphin

--- src/org/absct/animals/Dolphin.java
+++ src/org/absct/animals/Dolphin.java
@@ -1,5 +1,19 @@
 package org.absct.animals;
 
-public class Dolphin {
+public class Dolphin extends Animal implements Swimming{
     
+    @Override
+    public void sound(){
+        System.out.println("noi delfini ci avvaliamo di suoni per l'ecolocalizzazione, ma come le nostre parenti orche abbiamo un complesso sistema linguistico per comunicare tra di noi");
+    }
+
+    @Override
+    public void eat(){
+        System.out.println("fish");
+    }
+
+    @Override
+    public void swim(){
+        System.out.println("EHI GUARDAMI STO NUOTANDO VUOI  FARE UN GIRO?");
+    }
 }
--- src/org/absct/animals/Eagle.java
+++ src/org/absct/animals/Eagle.java
@@ -1,5 +1,19 @@
 package org.absct.animals;
 
-public class Eagle {
-    
+public class Eagle extends Animal implements Flying{
+
+    @Override
+    public void sound(){
+        System.out.println("chip");
+    }
+
+    @Override
+    public void eat(){
+        System.out.println("Sparrows");
+    }
+
+    @Override
+    public void fly(){
+        System.out.println("I'm volanding but better");
+    }
 }
