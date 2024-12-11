b55fb476866263a084cd8d1ea784ac57001e3799
AmerigoBovo97 <amerigobovo@gamil.com>
2024-12-11 22:22:21 +0100

    feat: implement App

--- src/org/absct/animals/App.java
+++ src/org/absct/animals/App.java
@@ -1,6 +1,32 @@
 package org.absct.animals;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Animal animal_1 = new Dog();
+        Dolphin animal_2 = new Dolphin();
+        Eagle animal_3 = new Eagle();
+
+        animal_1.sleep();
+        animal_1.sound();
+        animal_1.eat();
+
+        animal_2.sleep();
+        animal_2.sound();
+        animal_2.eat();
+
+        animal_3.sleep();
+        animal_3.sound();
+        animal_3.eat();
+
+        makeItFly(animal_3);
+        makeItSwim(animal_2);
+    }
+
+    static void makeItFly(Flying flying){
+        flying.fly();
+    }
+
+    static void makeItSwim(Swimming swimming){
+        swimming.swim();
     }
 }
